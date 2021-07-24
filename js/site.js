e8772b71c5b9058d75e17200f541d0175d886900
Matt <matthewtmuir@gmail.com>
2021-07-23 22:08:30 -0700

    Linking together JS with HTML

--- js/site.js
+++ js/site.js
@@ -1,4 +1,21 @@
-//Call Hello World
-function HelloWorld() {
-    alert("Hello World");
+
+
+
+//get values from the page
+function GetValues() {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    alert("The start value: " + startValue);
 }
+
+//generate nums from start val to end val
+function GenerateNums() {
+    
+}
+
+//display the numbers but EVEN numbers are in bold
+
+function DisplayNumbers() {
+    
+}
+
