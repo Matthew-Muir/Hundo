e1d526dee9c1cc646ead2f8c5b6ec7474ac25743
Matt <matthewtmuir@gmail.com>
2021-07-24 18:57:57 -0700

    Functional
    Works well. Found some UI things I don't love. Going to fix it in the template.

--- js/site.js
+++ js/site.js
@@ -1,21 +1,41 @@
-
-
-
 //get values from the page
 function GetValues() {
-    let startValue = document.getElementById("startValue").value;
-    let endValue = document.getElementById("endValue").value;
-    alert("The start value: " + startValue);
+  let startValue = document.getElementById("startValue").value;
+  let endValue = document.getElementById("endValue").value;
+
+  startValue = parseInt(startValue);
+  endValue = parseInt(endValue);
+
+  //Ensure values are integers
+  if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+    let numbers = GenerateNums(startValue, endValue);
+    DisplayNumbers(numbers);
+  }
+  //if not then give feedback
+  else {
+    alert("Must enter integers!");
+  }
 }
 
 //generate nums from start val to end val
-function GenerateNums() {
-    
+function GenerateNums(start, finish) {
+  let numbers = [];
+  for (let i = start; i <= finish; i++) {
+    numbers.push(i);
+  }
+  return numbers;
 }
+//Update html table element with data
+function DisplayNumbers(numbers) {
+  templateRows = "";
 
-//display the numbers but EVEN numbers are in bold
+  numbers.forEach((element) => {
+    if (element % 2 == 0) {
+      templateRows += "<tr><td><b>" + element.toString() + "<b></td></tr>";
+    } else {
+      templateRows += "<tr><td>" + element.toString() + "</td></tr>";
+    }
+  });
 
-function DisplayNumbers() {
-    
+  document.getElementById("results").innerHTML = templateRows;
 }
-
